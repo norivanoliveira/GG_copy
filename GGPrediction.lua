42f481a60e32a0dde732bf5ac8b9d7e40729a977
gamsteron <gamsteron@gmail.com>
2020-05-27 21:05:04 +0200

    Update GGPrediction.lua

--- GGPrediction.lua
+++ GGPrediction.lua
@@ -713,7 +713,7 @@ function Prediction:SpellPrediction(args)
         if not self.IsOnScreen then
             self.CastPosition = Math:Extended(self.MyHeroPos, Math:Normalized(self.CastPosition, self.MyHeroPos), 800)
         end
-        self.CastPosition.y = self.Target.pos.y
+        self.CastPosition.y = 0
         return true
     end
     function c:GetPrediction(target, source)
